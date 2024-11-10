4d8797f7ca09700031947a30d63f6e573323d0d3
Kristy <kristy.lee1218@outlook.com>
2024-11-09 20:43:23 -0600

    Offical project - added cpp and h file

--- CPP-TicTacToe/TicTacToe.cpp
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -3,10 +3,10 @@
 #include "TicTacToe.h"
 #include <iostream>
 
-//Game implementation 
+//Game implementation
 
-//Constructor to initialize the board 
-TicTacToe::TicTacToe() : m_currentPlayer(0)
+//Constructor to initialize or start the board
+TicTacToe::TicTacToe() : m_Player(0)
 {
 	for (int i = 0; i < 9; i++)
 	{
@@ -14,83 +14,76 @@ TicTacToe::TicTacToe() : m_currentPlayer(0)
 	}
 }
 
-//Function ovverides when the play has won or the board is full 
+//Function ovverides when the player has won or the board is full 
 bool TicTacToe::IsGameOver() const
 {
-	//Check winner for - horizontal
+	//Check winner for - horizontal win
 	if (m_board[0] == m_board[1] && m_board[1] == m_board[2] && m_board[0] != ' ')
 	{
-		std::cout << "Player" << m_board[0] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[0] << " is the winner!" << std::endl;
 		return true;
 	}
 	else if (m_board[3] == m_board[4] && m_board[4] == m_board[5] && m_board[3] != ' ')
 	{
-		std::cout << "Player" << m_board[3] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[3] << " is the winner!" << std::endl;
 		return true;
 	}
 	else if (m_board[6] == m_board[7] && m_board[7] == m_board[8] && m_board[6] != ' ')
 	{
-		std::cout << "Player" << m_board[6] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[6] << " is the winner!" << std::endl;
 		return true;
 	}
 
-	//Check winner for - vertical
+	//Check winner for - vertical win
 	if (m_board[0] == m_board[3] && m_board[3] == m_board[6] && m_board[0] != ' ')
 	{
-		std::cout << "Player" << m_board[0] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[0] << " is the winner!" << std::endl;
 		return true;
 	}
 	else if (m_board[1] == m_board[4] && m_board[4] == m_board[7] && m_board[1] != ' ')
 	{ 
-		std::cout << "Player" << m_board[1] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[1] << " is the winner!" << std::endl;
 		return true;
 	}
 	else if (m_board[2] == m_board[5] && m_board[5] == m_board[8] && m_board[2] != ' ')
 	{
-		std::cout << "Player" << m_board[2] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[2] << " is the winner!" << std::endl;
 		return true;
 	}
 
-	//Check winner for - diagonial wins
+	//Check winner for - diagonal win
 	if (m_board[0] == m_board[4] && m_board[4] == m_board[8] && m_board[0] != ' ')
 	{
-		std::cout << "Player" << m_board[0] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[0] << " is the winner!" << std::endl;
 		return true;
 	}
 	else if (m_board[2] == m_board[4] && m_board[4] == m_board[6] && m_board[2] != ' ')
 	{
-		std::cout << "Player" << m_board[2] << " WINNER!" << std::endl;
+		std::cout << "Player " << m_board[2] << " is the winner!" << std::endl;
 		return true;
 	}
 
-	//If the whole board is filled and no winner - it's a tie
+	//If the whole board is filled and no winner - Tie record
 	bool wholeBoard = true;
 	for (int i = 0; i < 9; i++)
 	{
+		//If the position is not 'X' or 'O'
 		if (m_board[i] != 'X' && m_board[i] != 'O')
 		{
 			wholeBoard = false;
 			break;
 		}
 	}
-
-	//// Only checking if the first cell is filled.
-	//bool wholeBoard = true;
-	//for (int i = 0; i < 9; i++)
-	//{
-	//	if (m_board[i] != 'X' && m_board[i] != 'O')
-	//		wholeBoard = false;
-	//	break;
-	//}
+	//If everything in the board is filled, display message "TIE"
 	if (wholeBoard)
 	{
-		std::cout << "It's a tie!" << std::endl;
+		std::cout << "The score is a tie!" << std::endl;
 		return true;
 	}
 	return false;
 }
 
-//Display the game to the console window 
+//Display the game to the console window - 3x3
 void TicTacToe::Display() const
 {
 	std::cout << "Let's play Tic Tac Toe!" << '\n';
@@ -110,29 +103,38 @@ void TicTacToe::Display() const
 void TicTacToe::TakeTurn()
 {
 	int number;
-	char key;
-
 	do
 	{
-		char Player = (m_currentPlayer % 2 == 0) ? 'X' : 'O';
+		//Display the character on the board as 'X' or 'O'
+		char Player = (m_Player % 2 == 0) ? 'X' : 'O';
+
+		//Display the player's turn and enter in a number
 		std::cout << "Player " << Player << " 's turn. Enter a spot to place a marker (1-9): ";
+
+		//Allow the player to type in the number value of 1-9
 		std::cin >> number;
 
-		//Verify that the input is valid  
-		if (number < 1 || number > 9 || m_board[number - 1] == 'X' || m_board[number - 1] == 'O')
+		//Verify that the input number is valid and within range of 1-9
+		if (number < 1 || number > 9)
 		{
-			std::cout << "Invalid move! Choose a empty spot between 1 and 9." << '\n';
+			std::cout << "Invalid move! Please choose a empty spot between 1 and 9." << '\n';
+		}
+		//Verify that the spot selected is an open spot 
+		else if (m_board[number - 1] == 'X' || m_board[number - 1] == 'O')
+		{
+			std::cout << "Spot is already choosen, please pick a different spot." << '\n';
 		}
 		else
 		{
+			//Place the player on the board
 			m_board[number - 1] = Player;
-			break;
+			break; //End the loop
 		}
 	}
 	while (true);
 
-	m_currentPlayer++;
+	//Allow the next player to play 
+	m_Player++;
 
-	
 }
 
--- CPP-TicTacToe/TicTacToe.h
+++ CPP-TicTacToe/TicTacToe.h
@@ -7,19 +7,20 @@ class TicTacToe : public Game
 
 private:
 	//Fields
-	char m_board[9]; //Allow the game board for 9 positions 
-	int m_currentPlayer;
+	char m_board[9]; //The position of the tictactoe board
+	int m_Player; //Display the character - 0 for 'X' and 1 for '0'
 
 public:
+	//Constructor - Intialize the game
 	TicTacToe();
 
 	//Check if the game is over
-	virtual bool IsGameOver() const override;
+	virtual bool IsGameOver() const;
 
 	//Allows the player to take turn
-	virtual void TakeTurn() override;
+	virtual void TakeTurn();
 
 	//Display the score
-	virtual void Display() const override;
+	virtual void Display() const;
 
 };
